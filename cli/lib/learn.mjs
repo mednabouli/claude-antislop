ef335bc2885d6db5b3db2977893693c2bdc43a8c
Med <mednabouli@gmail.com>
2026-09-07 22:45:46 -0400

    fix: remove unused ui, join imports from learn.mjs

--- cli/lib/learn.mjs
+++ cli/lib/learn.mjs
@@ -1,5 +1,4 @@
-import { ui } from './ui.mjs';
-import { dirname, join } from 'path';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
